b47a86453f080d1ae3d5cef68a2d056dce0e6578
unknown <walerius.kyllonen@aalto.fi>
2022-10-19 19:45:00 +0300

    userScreen kinda ready

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -3,23 +3,62 @@ import 'package:flutter/material.dart';
 
 import '../widgets/remembering_text_field.dart';
 
-class UserScreen extends StatefulWidget {
-  @override
-  State<UserScreen> createState() => _UserState();
-}
-
-class _UserState extends State<UserScreen> {
+class UserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: TopBar(),
-        body: Form(
-            child: Column(children: [
-          RememberingTextField("name"),
-          RememberingTextField("address"),
-          const Padding(
-              padding: EdgeInsets.all(16),
-              child: ElevatedButton(onPressed: null, child: Text("Save")))
-        ])));
+        body: SingleChildScrollView(child: Form(child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth < 600) {
+            return PhoneLayout();
+          } else if (constraints.maxWidth < 800) {
+            return LaptopLayout();
+          } else {
+            return DesktopLayout();
+          }
+        }))));
+  }
+}
+
+class PhoneLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(padding: const EdgeInsets.all(16), child: Settings());
+  }
+}
+
+class LaptopLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.grey[200],
+        padding: const EdgeInsets.all(16),
+        child: Settings());
+  }
+}
+
+class DesktopLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+            width: 800,
+            color: Colors.grey[200],
+            padding: const EdgeInsets.all(16),
+            child: Settings()));
+  }
+}
+
+class Settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      RememberingTextField("name"),
+      RememberingTextField("address"),
+      const Padding(
+          padding: EdgeInsets.all(16),
+          child: ElevatedButton(onPressed: (null), child: Text("Save")))
+    ]);
   }
 }
